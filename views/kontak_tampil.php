56e81f0028eff04caf7552997644d5ea9503e53a
putriandini12 <77843173+putriandini12@users.noreply.github.com>
2021-01-23 06:17:18 -0800

    Add files via upload

---
+++ views/kontak_tampil.php
@@ -0,0 +1,36 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="panel panel-danger">
+                <div class="panel-heading">
+                    <h3 class="panel-title" style="color: black; font-weight: bold;"> <span class="fa fa-user-plus"></span> Kontak</h3>
+                </div>
+                <div class="panel-body">
+                    <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                        <thead>
+                            <label class="col-sm-12 control-label"><center><strong>Sistem Informasi Pemilihan Tahfizh Terbaik</strong></center></label>
+                            <p align="center">
+                            Jl. Kisaran, Kec. Kota Kisaran Barat<br>
+                            Kabupaten Asahan, Sumatera Utara 21211,
+                            </p>
+                        </thead>
+                        <tbody>
+                          <div class="col-sm-2" align="center">
+
+                          </div>
+                          <div class="col-sm-8" align="center">
+                            <br>
+                            Telp. 082267065794<br>
+                            Email : putriandiniv1612@gmail.com<br>
+                            <br>
+                          </div>
+                          <div class="col-sm-2" align="center">
+                          </div>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
