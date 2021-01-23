a231af3a570d72db676717d48d01cfd7b25c2abb
putriandini12 <77843173+putriandini12@users.noreply.github.com>
2021-01-23 05:19:02 -0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            Copyright &COPY; <?= date('Y')?> Yayasan Tahfizh Kisaran | Mobile Programming | SI 7N | Putri Andini - Indah noviani - Rahmadhani
+        </div>
+    </div>
+</div>
